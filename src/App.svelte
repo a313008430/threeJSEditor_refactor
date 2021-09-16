27abe190f1568e14e12971e5d62b227f979fa143
xa <313008430@qq.com>
2021-09-16 10:34:38 +0800

    init‘

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import "./global.scss";
+    import Counter from "./lib/Counter.svelte";
+</script>
+
+<main>
+    <Counter />
+</main>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let count: number = 0;
+    const increment = () => {
+        count += 1;
+    };
+</script>
+
+<button
+    class=" py-4 px-8 bg-yellow-500 bg-opacity-10 rounded-3xl w-52 cursor-pointer focus:ring-2 focus:ring-yellow-500 text-yellow-600"
+    on:click={increment}
+>
+    Clicks: {count}
+</button>
