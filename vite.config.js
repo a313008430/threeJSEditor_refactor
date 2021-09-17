52491ad5e090a844868289c98758062162bd3235
xa <313008430@qq.com>
2021-09-17 13:43:38 +0800

    添加 vite-plugin-externals 库，用来过虑一些不需要打包的库

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { viteExternalsPlugin } from 'vite-plugin-externals';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte(), viteExternalsPlugin({
+    three: "THREE",
+  })],
+  build: {
+    rollupOptions: { external: ['three'] }
+  },
+  optimizeDeps: {
+    exclude: ['three']
+  },
+  server: {
+
+  }
+
 })
